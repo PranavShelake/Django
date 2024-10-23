c2d2ddbb8a4fc154c78466da912adb8dc804cd79
Pranav shelake <160570452+PranavShelake@users.noreply.github.com>
2024-10-23 16:24:03 +0530

    Update settings.py
    removed the swcurity code from the setting of email

--- Advance_login_form/Advance_login_form/settings.py
+++ Advance_login_form/Advance_login_form/settings.py
@@ -145,7 +145,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'eyesofegale01@gmail.com'
-EMAIL_HOST_PASSWORD = 'qnqenetyhwbruyfs'
+EMAIL_HOST_PASSWORD = '#######################'
 
 # Enforce session cookies to be sent only over HTTPS
 SESSION_COOKIE_SECURE = True
